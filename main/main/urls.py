55cc873884b9c0b6b5bce3a2d699df5cbe590025
GreggIsGreat <80446236+GreggIsGreat@users.noreply.github.com>
2022-12-02 13:36:50 +0200

    Portfolio App Django

--- main/main/urls.py
+++ main/main/urls.py
@@ -25,6 +25,4 @@ urlpatterns = [
     path('cap/', include('cap.urls')),
     path('reviews/', include('reviews.urls')),
 
-    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
